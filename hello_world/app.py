e21b1b7b76c0b255b65e08cfeb4ae7f390552122
Sam Keen <samkeen@amazon.com>
2019-12-10 10:34:29 -0800

    one way to fix

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,11 +1,10 @@
 import json
 
-import os, sys
-this_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(this_dir)
 
-from foo import goodbye
 
+print(f'__file__={__file__} | __name__={__name__} | __package__={str(__package__)}')
+
+from lib.foo import goodbye
 
 def lambda_handler(event, context):
     resp = "hello world, " + goodbye()
